4ba01636dd66f64246ecc7fe44d60bfd3e243749
azadorozhniy <artem.zadorozhniy@yetu.com>
2015-01-28 16:51:33 +0100

    Fixed bug on exp time validation

--- test/com/yetu/oauth2resource/services/tokenvalidation/OAuth2TokenValidationServiceTest.scala
+++ test/com/yetu/oauth2resource/services/tokenvalidation/OAuth2TokenValidationServiceTest.scala
@@ -6,8 +6,9 @@ import com.yetu.oauth2resource.utils.RoutesHelper
 import org.scalatest._
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.{Millis, Seconds, Span}
+import org.scalatest.MustMatchers._
 
-class OAuth2TokenValidationServiceTest extends BaseSpec with Matchers with RoutesHelper with ScalaFutures {
+class OAuth2TokenValidationServiceTest extends BaseSpec with Matchers with RoutesHelper with ScalaFutures  {
 
   implicit val defaultPatience =
     PatienceConfig(timeout =  Span(2, Seconds), interval = Span(5, Millis))
@@ -15,29 +16,34 @@ class OAuth2TokenValidationServiceTest extends BaseSpec with Matchers with Route
   val mockTokenValidationService = ValidationTestRegistry.tokenValidationService
   val realValidationService = ValidationServiceRegistry.tokenValidationService
 
-  "Validation service" should "reject dummy token after validation remote server" in {
+  "Validation service" must "reject dummy token after validation remote server" in {
     whenReady(mockTokenValidationService.validate(Some(wrongToken))) { either =>
-      either should be('left)
+      either must be('left)
     }
   }
 
-  "Validation service" should "accept correct token after validation remote server" in {
+  "Validation service" must "accept correct token after validation remote server" in {
     whenReady(mockTokenValidationService.validate(Some(correctToken))) { either =>
-      either should be('right)
+      either must be('right)
     }
   }
 
-  "Validation service" should "validate access token (jwt) locally with public key" in {
+  "Validation service" must "validate access token (jwt) locally with public key" in {
     whenReady(realValidationService.validate(Some(correctToken), JWTTokenMethod)) { either =>
-      either should be('right)
+      either must be('right)
     }
   }
 
-  "Validation service" should "validate reject wrong token (not jwt)" in {
+  "Validation service" must "reject wrong token (not jwt)" in {
     whenReady(realValidationService.validate(Some(wrongToken), JWTTokenMethod)) { either =>
-      either should be('left)
+      either must be('left)
     }
   }
 
-
+  "Validation service" must "validate access token (jwt) and make sure that JWT is valid at least for next month" in {
+    whenReady(realValidationService.validate(Some(correctToken), JWTTokenMethod)) { either =>
+      either must be('right)
+      either.right.get.exp.get must be > System.currentTimeMillis() / 1000 + (60 * 24 * 30)
+    }
+  }
 }
